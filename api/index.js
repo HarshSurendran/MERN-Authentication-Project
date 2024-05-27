a58c10b7eca66b9fb25cab6441079827b6a778bb
Harsh Surendran <harshsurendran@gmail.com>
2024-05-27 11:30:36 +0530

    Connect to mongodb database.

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to mongodb")
+}).catch((err)=>{
+    console.log("error while connecting to mongodb",err)
+});
 
 const app = express();
 
